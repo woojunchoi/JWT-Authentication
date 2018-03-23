409bcaa8abad1ec33f7e19e572c9c894954b5c09
woojunchoi <tim.woojune1@gmail.com>
2018-03-23 02:41:36 -0700

    json web token has been created and testing with mock data

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express();
+const jwt = require('jsonwebtoken')
 
 app.get('/', (req,res) => {
-    res.json(`<h1>there</h1>`)
+    res.json({
+        message:'welcome to API'
+    })
+})
+
+app.post('/api/posts', (req,res) => {
+   res.json({
+       message:'post created'
+   })
+})
+
+app.post('/api/login', (req,res) => {
+    const userinfo = {
+        id: 1,
+        username: 'tim',
+        email:'tim@gmail.com'
+    }
+    jwt.sign({user: username} ,'secret' ,(err,token )=>{
+        res.json({
+            token
+        })
+    })
 })
 
 app.listen(4000, () => {
